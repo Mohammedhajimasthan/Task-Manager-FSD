23a7874f656a4b9bfe5b872d72e17f969006dfb5
Mohammed Haji Masthan <152999674+Mohammedhajimasthan@users.noreply.github.com>
2026-03-28 15:16:33 +0530

    Update route.ts

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,1 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifyToken } from "@/middleware/authMiddleware";
 
+// GET all tasks
+export async function GET(req: NextRequest) {
+  try {
+    await verifyToken();
+
+    return NextResponse.json({
+      message: "All tasks fetched successfully",
+    });
+  } catch {
+    return NextResponse.json(
+      { message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+}
+
+// CREATE task
+export async function POST(req: NextRequest) {
+  try {
+    await verifyToken();
+
+    const { title, description, status = "pending" } = await req.json();
+
+    return NextResponse.json({
+      message: "Task created successfully",
+      task: { title, description, status },
+    });
+  } catch {
+    return NextResponse.json(
+      { message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+}
